a25d9fab56a1136e67a8ec8004e5a262729490d4
Easytooler <ah_kevin_xy@126.com>
2023-07-18 23:29:43 +0800

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,88 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Umax Admin',
+  tagline: '高效开源的 gin react admin 后台管理系统',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://umax-admin.github.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'umax-admin', // Usually your GitHub org/user name.
+  projectName: 'umax-admin.github.io', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  // i18n: {
+  //   defaultLocale: 'en',
+  //   locales: ['en'],
+  // },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+         // editUrl:
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'umax admin',
+        logo: {
+          alt: '好用的 框架',
+          src: 'img/logo.png',
+        },
+
+      },
+      footer: {
+        style: 'dark',
+        links: [
+
+
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Umax Admin, Inc`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
